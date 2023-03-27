1875c2989bf29022057aa9b50a2d804771b24e00
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 20:22:12 -0300

    Adicionado o arquivo 24 na pasata Capitulo02

---
+++ Capitulo 02/24 - Key_takeawy.py
@@ -0,0 +1,29 @@
+# Exercício 1
+#
+# Qual é o output do seguinte snippet?
+#
+print((2 ** 4), (2 * 4.), (2 * 4))
+#
+#
+# Verifique
+# 16 8.0 8
+#
+# Exercício 2
+#
+# Qual é o output do seguinte snippet?
+# 
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+#
+#
+# Verifique
+# -0.5 0.5 0 -1
+#
+# Exercício 3
+#
+# Qual é o output do seguinte snippet?
+#
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
+#
+#
+# Verifique
+# -2 2 512
