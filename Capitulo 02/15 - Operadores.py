2e9ad82fc4ff9a69e05e19f9b552a07bbaac8fb2
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 17:19:03 -0300

    Adicionado o arquivo 15 na pasta Capitulo 2

---
+++ Capitulo 02/15 - Operadores.py
@@ -0,0 +1,21 @@
+# Operadores básicos
+# Um operador é um símbolo da linguagem de programação, que é capaz de operar sobre os valores.
+#
+# Por exemplo, tal como na aritmética, o sinal + (mais) é o operador que é capaz de adicionar dois números, dando o resultado da adição.
+#
+# Mas nem todos os operadores Python são tão óbvios como o sinal de mais, por isso vamos analisar alguns dos operadores disponíveis em Python, e   
+# explicaremos que regras regem a sua utilização, e como interpretar as operações que realizam.
+#
+# Começaremos pelos operadores que estão associados às operações aritméticas mais amplamente reconhecidas:
+#
+# +, -, *, /, //, %, **
+#
+#
+# A ordem do seu aparecimento não é acidental. Falaremos mais sobre o assunto depois de termos passado por todos eles.
+#
+# Lembre-se: Os dados e os operadores, quando ligados entre si, formam expressões. A expressão mais simples é um literal em si.
+#
+print(2+2)
+print("2+2")
+print(-2+2)
+print(2*2+2-2)
