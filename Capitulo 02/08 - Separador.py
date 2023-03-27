be8b12b54f16698102430e0f91899d6edf7df353
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 11:29:46 -0300

    Adicionando o arquivo 08 na pasta 02

---
+++ Capitulo 02/08 - Separador.py
@@ -0,0 +1,3 @@
+# Agora a keyword sep="-" se torna um separador entre as strings.
+# Entre cada argumento da função print(), será colocado um seprador, nesse caso -
+print("My", "name", "is", "Monty", "Python.", sep="-")
