222bfe1f84ecdc989a82c54c0435e37d94503110
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 20:14:58 -0300

    Adicionado o arquivo 23 na pasta Capitulo02

---
+++ Capitulo 02/23 - Operadores_Parentises.py
@@ -0,0 +1,17 @@
+# Operadores e parêntesis
+# Claro, é sempre permitido utilizar parêntesis, o que pode alterar a ordem natural de um cálculo.
+#
+# De acordo com as regras aritméticas, as subexpressões entre parêntesis são sempre calculadas em primeiro lugar.
+#
+# Pode-se usar tantos parêntesis quantos forem necessários, e são frequentemente usados para melhorar a legibilidade de uma expressão, mesmo que não 
+# alterem a ordem das operações.
+#
+# Um exemplo de uma expressão com vários parêntesis é este:
+#
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+#
+# Tente calcular o valor que é impresso para a consola. Qual é o resultado da função print() ?
+#
+# Verifique
+# 10.0
+
