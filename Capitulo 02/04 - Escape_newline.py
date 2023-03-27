9123739cb5214b7382b7a6405bb2fd5174863472
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 10:59:48 -0300

    adicionado arquivo 04 na pasta 02

---
+++ Capitulo 02/04 - Escape_newline.py
@@ -0,0 +1,15 @@
+# 1. Se quiser colocar apenas uma barra invertida dentro de uma string, não se esqueça da sua natureza de escape -
+# tem de a duplicar, por exemplo, uma tal invocação causará um erro:
+#
+# print("\")
+#
+# enquanto esta não o fará:
+#
+# print("\\")
+#
+# 2.Nem todos os pares de escape (a barra invertida acoplada a outro caratere) significam algo.
+#
+
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
