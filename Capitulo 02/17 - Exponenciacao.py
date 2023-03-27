bcce3b72ae74addd32522752aec5cba774908a1c
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 17:23:06 -0300

    Adicionado o arquivo 17 na pasta Capitulo 02

---
+++ Capitulo 02/17 - Exponenciacao.py
@@ -0,0 +1,25 @@
+# Operadores aritméticos: exponenciação
+#
+# Um sinal ** (duplo asterisco) é um operador de exponenciação (potência). O seu argumento esquerdo é a base, o seu direito, o expoente.
+#
+# A matemática clássica prefere a notação com sobrescrito, tal como esta: 23. Os editores de texto puro não aceitam isso, por isso o Python usa ** ao 
+# invés, por exemplo, 2 ** 3.
+#
+# Dê uma vista de olhos aos nossos exemplos na janela do editor.
+#
+# Nota: rodeámos os asteriscos duplos com espaços nos nossos exemplos. Não é obrigatório, mas melhora a legibilidade do código.
+#
+# Os exemplos mostram uma característica muito importante de praticamente todos os operadores numéricos de Python.
+#
+# Execute o código e veja cuidadosamente os resultados que produz. Consegue ver alguma regularidade aqui?
+#
+# Lembre-se: É possível formular as seguintes regras com base neste resultado:
+#
+# quando ambos os argumentos ** são inteiros, o resultado é também um inteiro;
+# quando pelo menos um argumento ** é um float, o resultado é também um float.
+# Esta é uma distinção importante a lembrar.
+#
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
