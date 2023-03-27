9b9abc8a07a2dcdb812f641fb2fa237838a95f83
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 10:51:14 -0300

    Adicionado aruivo 02 na pasta 02

---
+++ Capitulo 02/02 - Linha_em_branco.py
@@ -0,0 +1,5 @@
+# Aqui vamos imprimir uma linha em branco com a função print()
+
+print("The itsy bitsy spider climbed up the waterspout.")
+print()
+print("Down came the rain and washed the spider out.")
