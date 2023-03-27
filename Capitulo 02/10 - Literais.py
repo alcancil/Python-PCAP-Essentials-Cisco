be6109b87e54392395dfc045c7642948e655a90c
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 12:28:13 -0300

    Adicionado o arquivo 10 na pasta 02

---
+++ Capitulo 02/10 - Literais.py
@@ -0,0 +1,13 @@
+# Um literal são dados cujos valores são determinados pelo próprio literal.
+# Exemplo:
+# 123 é um literal pois esse representa ele mesmo, ou cento e vinte três
+# Mas e c ?
+# Representa algum valor? Talvez. Pode ser o símbolo da velocidade da luz, por exemplo. Também pode ser a constante de integração. Ou mesmo o comprimento # de uma hipotenusa no sentido de um teorema de Pitágoras. Existem muitas possibilidades.
+#
+#
+print("2")
+print(2)
+#
+# Porquê as duas funções exibem o mesmo resultado ?
+# A primeira print("2") exibe o número 2 pois foi lhe passado um argumento, e por isso tem que estar entre aspas
+# Ja a segunda linha parece estar errada pois não fora utilizadas aspas. Mas o carácter 2 representa o número 2 e portnat, ele não necessita das aspas.
