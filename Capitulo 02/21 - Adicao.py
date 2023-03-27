f74ed562003220033a5e9a915c8d1e50e476d54f
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 17:42:11 -0300

    Adicionado o arquivo 20 na pasta Capitulo02

---
+++ Capitulo 02/21 - Adicao.py
@@ -0,0 +1,36 @@
+# Operadores: adição
+# O operador de adição é o sinal + (mais), que está totalmente de acordo com os padrões matemáticos.
+#
+# Novamente, dê uma vista de olhos no snippet do programa em baixo:
+# 
+# print(-4 + 4)
+# print(-4. + 8)
+#
+# O resultado não deve ser nada surpreendente. Execute o código para o verificar.
+#
+# O operador de subtração, operadores unários e binários
+# O operador de subtração é obviamente o sinal - (menos), embora deva notar que este operador também tem outro significado - ele pode alterar o sinal 
+# de um número.
+#
+# Esta é uma grande oportunidade para apresentar uma distinção muito importante entre operadores unários e binários.
+#
+# Em aplicações de subtração, o operador menos espera dois argumentos: o da esquerda (um minuendo em termos aritméticos) e o da direita (um 
+# subtraendo).
+#
+# Por esta razão, o operador de subtração é considerado um dos operadores binários, assim como os operadores de adição, multiplicação e divisão.
+#
+# Mas o operador menos pode ser usado de uma forma diferente (unária) - veja a última linha do snippet em baixo:
+#
+print(-4 - 4)
+print(4. - 8)
+print(-1.1)
+#
+# A propósito: há também um operador + unário. Pode utilizá-lo assim:
+#
+print(+2)
+#
+# O operador preserva o sinal de seu único argumento - o correto.
+#
+# Embora tal construção seja sintaticamente correta, a sua utilização não faz muito sentido, e seria difícil encontrar uma boa razão para o fazer.
+#
+# Dê uma vista de olhos no snippet acima - consegue adivinhar o seu output?
