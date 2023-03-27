785c577ef1eb61f8ac8c28cc23ecd96c5405adbb
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 15:30:29 -0300

    Adicionado arquivo 13 na pasta 02

---
+++ Capitulo 02/13 - Strings.py
@@ -0,0 +1,6 @@
+# As strings são utilizadas dentro de aspas " string "
+# Mas como imprimir a frase : I like "Monty Python"
+# Bom podemos utilizar o caracter de escape \# ou mesmo ' string "Frase" '
+
+print("I like \"Monty Python\"")
+print('I like "Monty Python"')
