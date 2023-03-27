d2ed05911148391c0a203ce581fcad4915fe0d87
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 17:26:40 -0300

    Adicionado o arquivo 18 na pasta Capítulo02

---
+++ Capitulo 02/18 - Multiplicacao_Divisao.py
@@ -0,0 +1,32 @@
+# Operadores aritméticos: multiplicação
+# Um sinal * (asterisco) é um operador de multiplicação.
+#
+# Execute o código abaixo e verifique se a nossa regra de inteiro vs. float ainda está a funcionar.
+#
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+#
+#
+# Operadores aritméticos: divisão
+# O sinal / (barra) é um operador de divisão.
+#
+# O valor em frente da barra é um dividendo, o valor por detrás da barra, um divisor.
+#
+# Execute o código abaixo e analise os resultados.
+#
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+#
+#
+# Deve ver que existe uma exceção à regra.
+#
+# O resultado produzido pelo operador da divisão é sempre um float, independentemente de o resultado parecer ou não ser um float à primeira vista: 1 / # 2, ou se se parecer com um inteiro puro: 2 / 1.
+#
+# Isto é um problema? Sim, é. Acontece por vezes que é realmente necessária uma divisão que forneça um valor inteiro, não um float.
+#
+# Felizmente, o Python pode ajudá-lo com isso.
+
