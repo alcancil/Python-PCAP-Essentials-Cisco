a17f3d80bb052766349249f0d398b1043fcada80
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 11:45:33 -0300

    Adicionado o arquivo 09 na pasta 02

---
+++ Capitulo 02/09 - Keyword_sep.py
@@ -0,0 +1,4 @@
+# É possivel se utilizar um keyword e um separador juntos.
+# Aquie temos um exemplo do sep="_" + end="*" e sep="*" + end="*\n"
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
