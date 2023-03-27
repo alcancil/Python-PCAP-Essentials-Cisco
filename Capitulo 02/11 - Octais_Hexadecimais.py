0c1b01d2bd818e67b40a2adb2ba9697b84683910
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 12:43:36 -0300

    Adicionado o arquivo 11 na pasta 02

---
+++ Capitulo 02/11 - Octais_Hexadecimais.py
@@ -0,0 +1,17 @@
+# A função printe aceita literais dos tipos: inteiro, octais e hexadecimais
+# Para se utilizar números inteiros basta colocar eles dentro das chaves ( ) . OBS: nunca usar o 0 no ínicio.
+# Para se utilizar números octais, utilizar (0o sequência de digitos de 0 a 7)
+# Para se utilizar números hexadecimais, utilizar ( 0x sequencia de digitos entre 0 a 9 e letras de A a F)
+# OBS: o python já converte automaticamente o resultado para decimal e imprime na tela
+#
+# imprimir inteiros
+print(123)
+#
+# imprimir octais
+print(0o123)
+print(0o01234567)
+#
+# imprimir hexadecimais
+print(0x0123456789ABCDEF)
+print(0x123)
+
