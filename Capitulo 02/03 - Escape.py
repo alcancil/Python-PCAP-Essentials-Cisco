e4cca16680652d315ed126e8e787d3d8884ac13e
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-27 10:54:45 -0300

    adicionado arquivo 03 na pasta 02

---
+++ Capitulo 02/03 - Escape.py
@@ -0,0 +1,6 @@
+# A barra invertida (\) tem um significado muito especial quando usado dentro de strings - a isto chama-se o caratere de escape.
+# Aqui vamos utilizar o \n dentro da função print( ) . Com isso, o print() interprete como uma nova linha
+
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
