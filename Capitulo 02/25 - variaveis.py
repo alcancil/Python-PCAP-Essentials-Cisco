c85a84250865a3ea5073a5a1a59b014d3ea0d6c8
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-28 11:34:44 -0300

    Adicionado o arquivo 25 na pasta Capitulo 02

---
+++ Capitulo 02/25 - variaveis.py
@@ -0,0 +1,10 @@
+# Variaveis
+# Variaveis são utilizadas para armazenar algum conteudo. Ela pode ser um simples número, uma string ou mesmo o resultado de uma conta.
+# Obs: para a criação dos nomes das variavies vale diser que elas são case sensitive. Existem palavras reservadas como import que não pode ser usadas
+#
+var = 1
+VAR = 2
+
+print(var)
+print(VAR)
+print(var + VAR)
