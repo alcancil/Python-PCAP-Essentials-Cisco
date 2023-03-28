57b1110ef4a1898338b4738a2595fcb8af8c4875
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-28 16:00:46 -0300

    Adicionado o arquivo 31 a pasta Capitulo 02

---
+++ Capitulo 02/31 - Input.py
@@ -0,0 +1,8 @@
+# Função Input()
+#
+# A Função Input() serve como um complemento a função print(). Enquanto a função print() imprimi na tela os argumentos fornecidos, a função input()
+# fica esperando o usuário entrar com os dados através do teclado, por exemplo
+#
+print("Tell me anything...")
+anything = input()
+print("Hmm...", anything, "... Really?")
