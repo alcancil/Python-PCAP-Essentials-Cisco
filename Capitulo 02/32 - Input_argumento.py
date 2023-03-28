38a2bb1de349ce6c2f4fb39217db17d1630d6eda
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-28 16:21:20 -0300

    Adicionado o arquivo 32 a pasta Capitulo 02

---
+++ Capitulo 02/32 - Input_argumento.py
@@ -0,0 +1,7 @@
+# Função input pode receber um argumento ao invés de esperar o usuário entrar com o mesmo. Por exemplo:
+#
+anything = input("Tell me anything...")
+print("Hmm...", anything, "...Really?")
+#
+# Mas cabe aqui ressaltare que o argumento fornecido para a função input sempre sera uma string.
+# Portanto não é possível realizar algum tipo de cálculo somente dessa forma
