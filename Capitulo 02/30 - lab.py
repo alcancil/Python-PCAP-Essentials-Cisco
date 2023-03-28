f7b846db758e6b3acbddb4578a71f52c4710efb3
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-28 15:28:27 -0300

    Adicionado o arquivo 30 a pasta Capitulo 02

---
+++ Capitulo 02/30 - lab.py
@@ -0,0 +1,44 @@
+# LAB
+#
+# Tempo estimado
+# 10-15 minutos
+#
+# Nível de dificuldade
+# Fácil
+# 
+# Objetivos
+# familiarizar-se com o conceito de números, operadores, e operações aritméticas em Python;
+# realizar cálculos básicos.
+# Cenário
+# Veja o código no editor: ele lê um valor float , coloca-o numa variável chamada x, e imprime o valor de uma variável chamada y. A sua tarefa é 
+# completar o código para avaliar a seguinte expressão:
+#
+# 3x3 - 2x2 + 3x - 1
+#
+# O resultado deve ser atribuído a y.
+#
+# Lembre-se que a notação algébrica clássica gosta de omitir o operador de multiplicação - precisa de o utilizar explicitamente. Note como alteramos o # tipo de dados para garantir que x é do tipo float.
+#
+# Mantenha o seu código limpo e legível, e teste-o usando os dados que fornecemos, atribuindo-o cada vez à variável x (por hardcoding). Não desanime 
+# por quaisquer falhas iniciais. Seja persistente e inquisitivo.
+#
+#
+# Dados de teste
+# Input de amostra
+#
+# x = 0
+# x = 1
+# x = -1
+#
+# Output esperado
+#
+# y = -1.0
+# y = 3.0
+# y = -9.0
+# output
+
+x = -1 # hardcode your test data here
+x = float(x)
+# write your code here
+y = ( 3*x**3 - 2*x**2 + 3*x - 1)
+print("y =", y)
