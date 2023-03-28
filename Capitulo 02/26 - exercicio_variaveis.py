8efb2b3b8464224722dffdc8c89ad4457f94fc64
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-28 11:45:15 -0300

    Adicionado o arquivo 26 na pasta Capitulo 02

---
+++ Capitulo 02/26 - exercicio_variaveis.py
@@ -0,0 +1,11 @@
+# Resolução de problemas matemáticos simples
+# Agora deverá ser capaz de construir um programa curto que resolva problemas matemáticos simples, como o teorema de Pitágoras:
+#
+# O quadrado da hipotenusa é igual à soma dos quadrados dos catetos.
+#
+# O código seguinte avalia o comprimento da hipotenusa (ou seja, o lado mais longo de um triângulo retângulo, o oposto ao ângulo reto) usando o teorema de # Pitágoras:
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
