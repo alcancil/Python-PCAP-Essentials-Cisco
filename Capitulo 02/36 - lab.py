32c0e435f915c59ed68cc3ac4b24db9c971f64e7
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-28 20:46:50 -0300

    Adicionado o arquivo 36 na pasta Capitulo 02

---
+++ Capitulo 02/36 - lab.py
@@ -0,0 +1,40 @@
+# LAB
+# 
+# Tempo estimado
+# 5-10 minutos
+#
+# Nível de dificuldade
+# Fácil
+#
+# Objetivos
+# familiarizar-se com o input e output de dados em Python;
+# avaliar expressões simples.
+# Cenário
+# A sua tarefa é completar o código a fim de avaliar os resultados de quatro operações aritméticas básicas.
+#
+# Os resultados têm de ser impressos para a consola.
+#
+# Poderá não conseguir proteger o código de um utilizador que queira dividir por zero. Tudo bem, não se preocupe com isso por agora.
+#
+# Teste o seu código - produz os resultados que espera?
+#
+# Não lhe mostraremos quaisquer dados de teste - isso seria demasiado simples.
+
+
+# input a float value for variable a here
+# input a float value for variable b here
+
+# output the result of addition here
+# output the result of subtraction here
+# output the result of multiplication here
+# output the result of division here
+
+a = 1.0
+b = 2.0
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+print("\nThat's all, folks!")
