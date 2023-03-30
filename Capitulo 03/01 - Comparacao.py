de0ab904fa8e716290b86c8a5dd807719e219378
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-30 16:39:36 -0300

    Adicionado a pasta Capitulo 03

---
+++ Capitulo 03/01 - Comparacao.py
@@ -0,0 +1,37 @@
+# Comparação: operador de igualdade
+# Pergunta: são dois valores iguais?
+#
+# Para fazer esta pergunta, utiliza o operador == (igual igual).
+#
+# Não se esqueça desta importante distinção:
+# 
+# = é um operador de atribuição, por exemplo, a = b atribui a com o valor de b;
+# == é a questão são estes valores iguais?; a == b compara a e b.
+# É um operador binário com ligação do lado esquerdo. Precisa de dois argumentos e verifica se são iguais.
+#
+#
+# Exercícios
+# Agora vamos fazer algumas perguntas. Tente adivinhar as respostas.
+#
+# Pergunta #1: Qual é o resultado da seguinte comparação?
+#
+# 2 == 2    Verifique
+# 
+# True - claro, 2 é igual a 2. O Python responderá True (lembre-se deste par de literais predefinidos, True e False - são também keywords de Python).
+# 
+# Pergunta #2: Qual é o resultado da seguinte comparação?
+# 
+# 2 == 2.    Verifique
+# 
+# Esta questão não é tão fácil quanto a primeira. Felizmente, o Python é capaz de converter o valor inteiro no seu verdadeiro equivalente, e 
+# consequentemente, a resposta é True.
+#
+# Pergunta #3: Qual é o resultado da seguinte comparação?
+#
+# 1 == 2    Verifique
+#
+# Esta deve ser fácil. A resposta será (ou melhor, será sempre) False.
+
+print(1 == 1)
+print(2 == 2.) 
+print(1 == 2)
