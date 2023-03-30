7162fcc0bc6102c827b610a4d68af103283d3f4c
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-30 16:52:57 -0300

    Adicionado o arquivo 02 na pasta Capitulo 03

---
+++ Capitulo 03/02 - Igualdade_desigualdade.py
@@ -0,0 +1,19 @@
+# Igualdade
+#
+# A igualdade é representado por dois simbolos de igual ==
+# Ele serve para fazer a comparação de dois termos tendo como saída True ou False
+#
+var = 0
+print( "Var recebe o valor de 0" )
+print( "A variavel var e iguala a 0 ? ", var == 0 )
+print( "A variavel var e iguala a 1 ? ", var == 1 )
+#
+# Desigualdade
+#
+# A desigualdade é representada pelos caracteres !=
+# Ela é o contrario da igualdade e também tem as saídas como True ou False
+#
+var = 0
+print( "Var recebe valor de 0" )
+print( "A variavel var e diferente de 0 ?", var !=0)
+print( "A variavel var e diferente de 1 ?", var !=1)
