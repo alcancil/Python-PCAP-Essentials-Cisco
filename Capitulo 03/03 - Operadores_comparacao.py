afe7e4de3ccc4a0dd6a53b43e7b7a27ff9d8486f
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-03-30 18:06:28 -0300

    Adicionado o arquivo 03 na pasta Capitulo03

---
+++ Capitulo 03/03 - Operadores_comparacao.py
@@ -0,0 +1,16 @@
+# Existem dois operadores de comparação
+# >= maior ou igual a 
+# <= menor ou igaul a 
+# Exemplos
+var = 1
+print("1 - maior ou igal a 1 ?", 1 >= var)
+print("2 - maior ou igal a 1 ?", 2 >= var)
+print("0 - maior ou igal a 1 ?", 0 >= var)
+
+# Prioridade	Operador	
+# 1	              +, -	unário
+# 2	              **	
+# 3	              *, /, //, %	
+# 4	              +, -	binário
+# 5	              <, <=, >, >=	
+# 6	              ==, !=	
