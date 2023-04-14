44c05f6a5334e063fa34db9120e922a10aaf9f4c
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-04-14 17:06:15 -0300

    Adicionado o arquivo 27 na pasta Capitulo 03 . Rever aqrquivos 25 e 26

---
+++ Capitulo 03/27 - While_else.py
@@ -0,0 +1,11 @@
+# Loop While
+#
+# Como mostrado anteriormente com o if, o while também poder ter o uso do else. Veja o exemplo
+#
+#
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("else:", i)
