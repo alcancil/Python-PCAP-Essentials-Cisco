e5c5423e1dcf82dba3a71637e25319d34790b6e6
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-04-17 19:33:57 -0300

    Adicionado o arquivo 42 na pasta Capítulo 03

---
+++ Capitulo 03/42 - Indices_Neg_lista.py
@@ -0,0 +1,23 @@
+# Índices negativos são legais
+# Pode parecer estranho, mas os índices negativos são legais, e podem ser muito úteis.
+#
+# Um elemento com um index igual a -1 é o último na lista.
+#
+# print(numbers[-1])
+#
+#
+# O snippet de exemplo terá como output 1. Execute o programa e verifique.
+#
+#
+# Da mesma forma, o elemento com um index igual a -2 é o penúltimo na lista.
+#
+# print(numbers[-2])
+#
+#
+# O snippet de exemplo terá como output 2.
+# 
+# O último elemento acessível da nossa lista é numbers[-4] (o primeiro) - não tente ir mais longe!
+#
+numbers = [111, 7, 2, 1]
+print(numbers[-1])
+print(numbers[-2])
