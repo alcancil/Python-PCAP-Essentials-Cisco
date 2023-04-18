e4ee348a737cc3ed6588eaada42cd952fa4c2fc5
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-04-18 15:42:04 -0300

    Adicionado o arquivo 57 na pasta Capitulo 03

---
+++ Capitulo 03/57 - Listas_fatias_indices_negativos.py
@@ -0,0 +1,34 @@
+# Slices - índices negativos
+# Veja o snippet em baixo:
+#
+# my_list[start:end]
+#
+#
+# Para repetir:
+#
+# start é o index do primeiro elemento incluído no slice;
+# end é o index do primeiro elemento não incluído no slice.
+#
+# É assim que os índices negativos funcionam com o slice:
+
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:-1]
+print(new_list)
+
+
+# O output do snippet é:
+#
+# [8, 6, 4]
+# output
+#
+#
+# Se o start especifica um elemento que se encontra mais longe do que o descrito pelo end (do ponto de vista inicial da lista), o slice estará vazio:
+
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[-1:1]
+print(new_list)
+
+
+# O output do snippet é:
+#
+# []
