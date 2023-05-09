9e1d8ff5cde84fc70c8e019ce67e3704d3d2221b
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-09 18:20:44 -0300

    Adicionado o arquivo 01 - Modulos03.py na pasta Capitulo 01

---
+++ PCAP 2/Capitulo 01/01 - Modulos03.py
@@ -0,0 +1,12 @@
+# Modulos
+#
+# Ja vimos que e possivel se criar um arquivo de modulos contendo funcoes, constantes, etc, para que possam ser importados dentro do codigo principal. Para isso, realizamos a importacao da seguinte maneira
+# import utils. Mas isso tem um pequeno problema. Se o arquivo utils for grande, dessa forma estamos importando todo seu conteudo e assim, podemos causar algum impacto dentro da pareformance do programa final
+# E se quisermos importar somete alguma funcao de dentro desse modulo por exemplo ? Veja o exemplo:
+
+
+from utils import soma
+
+print(soma(2,3))
+
+# Perceba que agora importamos somente a funcao soma e deixamos sem importar a funcao divisao e nem a constatnet idade. 
