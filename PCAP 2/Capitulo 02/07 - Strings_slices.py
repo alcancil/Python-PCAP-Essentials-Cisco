bc98181459d2ccc07a3133ffff22a18927e7871d
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-12 17:37:00 -0300

    Adicionado o arquivo 07 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/07 - Strings_slices.py
@@ -0,0 +1,20 @@
+# Slices
+#
+# Além disso, tudo o que sabe sobre slices ainda é utilizável.
+#
+# Reunimos alguns exemplos que mostram como as slices funcionam no mundo das strings. 
+# Olhe para o código no editor, analise-o e execute-o.
+#
+# Não verá nada de novo no exemplo, mas queremos que tenha a certeza de que pode explicar todas as linhas do código.
+
+# Slices
+
+alpha = "abdefg"
+
+print(alpha[1:3])
+print(alpha[3:])
+print(alpha[:3])
+print(alpha[3:-2])
+print(alpha[-3:4])
+print(alpha[::2])
+print(alpha[1::2])
