8ff319997e77d44dac6aeaa57355b26a68b1d9d8
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-12 20:39:03 -0300

    Adicionado o arquivo 12 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/12 - Strings_max.py
@@ -0,0 +1,31 @@
+# Operações em strings: max()
+#
+# Da mesma forma, uma função chamada max() encontra o elemento máximo da sequência.
+#
+# Veja o Exemplo 1 no editor. O output do programa de exemplo:
+# z
+#
+# output
+#
+# Nota: É um z minúsculo.
+#
+# Agora vamos ver a função max() aplicada aos mesmos dados que anteriormente. Veja os Exemplos 2 & 3 no editor.
+#
+# O output esperado é:
+# [y]
+# 2
+#
+# output
+#
+# Realize as suas próprias experiências.
+
+# Demonstrating max() - Example 1:
+print(max("aAbByYzZ"))
+
+
+# Demonstrating max() - Examples 2 &amp; 3:
+t = 'The Knights Who Say "Ni!"'
+print('[' + max(t) + ']')
+
+t = [0, 1, 2]
+print(max(t))
