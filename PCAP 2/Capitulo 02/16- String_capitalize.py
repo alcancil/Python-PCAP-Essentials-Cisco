b8c7ebd2323bcb3de15201c5350add4a414813bc
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-13 11:15:53 -0300

    Modificado o arquivo 16 na pasta Capitulo 02

--- PCAP 2/Capitulo 02/16- String_capitalize.py
+++ PCAP 2/Capitulo 02/16- String_capitalize.py
@@ -24,3 +24,9 @@
 #
 # Demonstrating the capitalize() method:
 print('aBcD'.capitalize())
+
+print("Alpha".capitalize())
+print('ALPHA'.capitalize())
+print(' Alpha'.capitalize())
+print('123'.capitalize())
+#print("αβγδ".capitalize())
