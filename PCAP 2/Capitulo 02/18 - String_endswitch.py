30e2baf539662660c92b9315aec399c59b518df5
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-13 11:25:07 -0300

    Adicionado o arquivo 18 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/18 - String_endswitch.py
@@ -0,0 +1,28 @@
+# Os loops endswith() .
+#
+# A classe endswith() verifica se a string dada termina com o argumento especificado e devolve True ou False, dependendo do resultado da verificação.
+#
+# Nota: a substring deve aderir ao último caratere da string - não pode estar localizada apenas algures perto do final da string.
+#
+# Veja o nosso exemplo no editor, analise-o, e execute-o. O seu output é:
+# yes
+#
+# Demonstrating the endswith() method:
+if "epsilon".endswith("on"):
+    print("yes")
+else:
+    print("no")
+
+
+
+# output
+#
+# Deverá agora ser capaz de prever o output do snippet abaixo:
+t = "zeta"
+print(t.endswith("a"))
+print(t.endswith("A"))
+print(t.endswith("et"))
+print(t.endswith("eta"))
+
+
+# Execute o código para verificar as suas previsões.
