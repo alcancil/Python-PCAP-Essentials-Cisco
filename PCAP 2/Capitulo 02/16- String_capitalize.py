8595b36eb99e2d9e52b997266ffa2f24d1fef0ee
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-13 11:12:16 -0300

    Adicionado o arquivo 16 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/16- String_capitalize.py
@@ -0,0 +1,26 @@
+# Os loops capitalize() .
+#
+# Vamos passar por alguns métodos padrão de string Python. Vamos atravessá-los por ordem alfabética - para ser honesto, qualquer ordem tem tantas desvantagens como vantagens, pelo que a escolha pode também 
+# ser aleatória.
+#
+# O método capitalize() faz exatamente o que diz - cria uma nova string cheia de carateres retirados da source string, mas tenta modificá-los da seguinte forma:
+#
+#    se o primeiro caratere dentro da string for uma letra (nota: o primeiro caratere é um elemento com um index igual a 0, não apenas o primeiro caratere visível), será convertido para maiúsculas;
+#    todas as letras restantes da string serão convertidas em minúsculas.
+#
+# Não se esqueça:
+#
+#    a string original (da qual o método é invocado) não é alterada de forma alguma (a imutabilidade de uma string deve ser obedecida sem reservas)
+#    a string modificada (capitalizada neste caso) é devolvida como resultado - se não a utilizar de qualquer forma (atribuí-la a uma variável, ou passá-la a uma função/método) ela desaparecerá sem deixar 
+# rasto.
+#
+# Nota: os métodos não têm de ser invocados apenas de dentro das variáveis. Podem ser invocados diretamente de dentro de literais de string. Vamos utilizar regularmente essa convenção - simplificará os 
+# exemplos, uma vez que os aspetos mais importantes não desaparecerão entre as tarefas desnecessárias.
+#
+# Veja o exemplo no editor. Execute-o.
+#
+# Isto é o que ele imprime:
+# Abcd
+#
+# Demonstrating the capitalize() method:
+print('aBcD'.capitalize())
