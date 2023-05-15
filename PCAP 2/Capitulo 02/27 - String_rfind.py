1a8c54f326ffdb9dbe2eed18e9113e28589b0548
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-15 14:21:43 -0300

    Adicionado o arquivo 27 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/27 - String_rfind.py
@@ -0,0 +1,13 @@
+# rfind Python () retorna a posição da última ocorrência da cadeia, se não houver correspondência -1 é retornado.
+#
+# Os loops rfind() .
+#
+# Os métodos de um, dois e três parâmetros chamados rfind() fazem quase as mesmas coisas que os seus homólogos (os desprovidos do prefixo r), mas começam as suas buscas a partir do fim da string, não do 
+# início (daí o prefixo r, para right).
+#
+# Veja o exemplo do código no editor e tente prever o seu output. Execute o código para verificar se estava certo.
+#
+# Demonstrating the rfind() method:
+print("tau tau tau".rfind("ta"))
+print("tau tau tau".rfind("ta", 9))
+print("tau tau tau".rfind("ta", 3, 9))
