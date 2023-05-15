57e1e2ebd89dfe484869ff906d31b03f11c30f1e
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-15 14:42:23 -0300

    Adicionado o arquivo 30 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/30 - String_startwith_strip.py
@@ -0,0 +1,28 @@
+# Os loops startswith() .
+#
+# A classe startswith() é um reflexo espelhado de endswith() - verifica se uma determinada string começa com a substring especificada.
+#
+# Veja o exemplo no editor. Este é o resultado:
+# False
+# True
+#
+# output
+# O método strip() .
+#
+# A classe trip() combina os efeitos causados por rstrip() e lstrip() - faz uma nova string com falta de todos os espaços em branco à esquerda e à direita.
+#
+# Veja o segundo exemplo no editor. Este é o resultado que ele devolve:
+# [aleph]
+#
+# output
+#
+# Agora, realize as suas próprias experiências com os dois métodos.
+#
+# Demonstrating the startswith() method:
+print("omega".startswith("meg"))
+print("omega".startswith("om"))
+
+print()
+
+# Demonstrating the strip() method:
+print("[" + "   aleph   ".strip() + "]")
