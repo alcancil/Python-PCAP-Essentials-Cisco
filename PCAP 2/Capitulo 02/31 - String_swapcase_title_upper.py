e488a58fb32ccd132a3cdaf2162e0453e1c7b1b8
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-15 14:46:49 -0300

    Adicionado o arquivo 31 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/31 - String_swapcase_title_upper.py
@@ -0,0 +1,38 @@
+# Os loops swapcase() .
+#
+# A classe swapcase() faz uma nova string, trocando a maiúscula/minúscula de todas as letras dentro da source string: 
+# carateres minúsculos tornam-se maiúsculas e vice-versa.
+#
+# Todos os outros carateres permanecem intocados.
+#
+# Veja o primeiro exemplo no editor. Consegue adivinhar o output? Não terá bom aspeto, mas tem de o ver:
+# i KNOW THAT i KNOW NOTHING.
+#
+# output
+# O método title() .
+#
+# A classe title() desempenha uma função algo semelhante - muda a primeira letra de cada palavra para maiúscula, transformando todas as outras em minúsculas.
+#
+# Veja o segundo exemplo no editor. Consegue adivinhar o seu output? Este é o resultado:
+# I Know That I Know Nothing. Part 1.
+#
+# output
+# O método upper() .
+#
+# Por último, mas não menos importante, o método upper() faz uma cópia da source string, substitui todas as letras minúsculas pelas suas contrapartes maiúsculas, e devolve a string como resultado.
+#
+# Veja o terceiro exemplo no editor. O seu output é:
+# I KNOW THAT I KNOW NOTHING. PART 2.
+#
+# Demonstrating the swapcase() method:
+print("I know that I know nothing.".swapcase())
+
+print()
+
+# Demonstrating the title() method:
+print("I know that I know nothing. Part 1.".title())
+
+print()
+
+# Demonstrating the upper() method:
+print("I know that I know nothing. Part 2.".upper())
