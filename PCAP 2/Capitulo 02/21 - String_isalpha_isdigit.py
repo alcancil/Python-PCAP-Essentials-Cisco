c6434a900b8cc76a329b106c0c8fde5c7312d3cd
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-15 13:31:05 -0300

    Adicionado o arquivo 21 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/21 - String_isalpha_isdigit.py
@@ -0,0 +1,28 @@
+# Os loops isalpha() .
+#
+# A classe isalpha() é mais especializado - está interessado apenas em letras.
+#
+# Veja o Exemplo 1 - o seu output é:
+# True
+# False
+#
+# output
+# O método isdigit() .
+# 
+# Por sua vez, o método isdigit() olha apenas para os dígitos - qualquer outra coisa produz False como o resultado.
+#
+# Veja o Exemplo 2 - o seu output é:
+# True
+# False
+#
+# output
+#
+# Realize mais experiências.
+#
+# Example 1: Demonstrating the isapha() method:
+print("Moooo".isalpha())
+print('Mu40'.isalpha())
+
+# Example 2: Demonstrating the isdigit() method:
+print('2018'.isdigit())
+print("Year2019".isdigit())
