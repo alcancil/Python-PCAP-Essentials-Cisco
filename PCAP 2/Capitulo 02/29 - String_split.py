00fd7eb5e2be16d17131e050f59509c33b050622
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-15 14:38:39 -0300

    Adicionado o arquivo 29 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/29 - String_split.py
@@ -0,0 +1,18 @@
+# Os loops split() .
+#
+# A classe split() faz o que diz - divide a string e constrói uma lista de todas as substrings detetadas.
+#
+# O método assume que as substrings são delimitadas por espaços em branco - os espaços não participam na operação, e não são copiados para a lista resultante.
+#
+# Se a string estiver vazia, a lista resultante também estará vazia.
+#
+# Veja o código no editor. O exemplo produz o seguinte output:
+# ['phi', 'chi', 'psi']
+#
+# output
+#
+# Nota: a operação inversa pode ser realizada pelo método join() 
+
+
+# Demonstrating the split() method:
+print("phi       chi\npsi".split())
