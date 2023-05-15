e27f90364e756c53ba0a2d3a36002238bdafde8b
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-15 14:36:05 -0300

    Adicionado o arquivo 28 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/28 - String_rstrip.py
@@ -0,0 +1,11 @@
+# Os loops rstrip() .
+#
+# Duas variantes do método rstrip() fazem quase o mesmo que lstrip, mas afetam o lado oposto da string.
+#
+# Veja o exemplo de código no editor. Consegue adivinhar o seu output? Execute o código para verificar as suas suposições.
+#
+# Como habitualmente, encorajamo-lo a experimentar os seus próprios exemplos.
+
+# Demonstrating the rstrip() method:
+print("[" + " upsilon ".rstrip() + "]")
+print("cisco.com".rstrip(".com"))
