e455a6669fcbde271b51add5187e4b2b3c2dddd3
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-15 13:42:26 -0300

    Adicionado o arquivo 24 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/24 - String_lower.py
@@ -0,0 +1,18 @@
+# Os loops lower() .
+#
+# A classe lower() faz uma cópia de uma source string, substitui todas as letras maiúsculas por minúsculas, e devolve a # string como resultado. Novamente, a source string permanece intocada.
+#
+# Se a string não contiver carateres maiúsculos, o método devolve a string original.
+#
+# Nota: O método lower() não toma nenhum parâmetro.
+#
+# O exemplo no editor tem como output:
+# sigma=60
+#
+# output
+#
+# Como de costume, realize as suas próprias experiências.
+
+
+# Demonstrating the lower() method:
+print("SiGmA=60".lower())
