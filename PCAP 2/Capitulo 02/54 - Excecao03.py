d8fcecfddc7bcba4141580aa4af7c7d7fe5a2de2
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-18 21:49:16 -0300

    Adicionado o arquivo 54 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/54 - Excecao03.py
@@ -0,0 +1,16 @@
+# Exceções: continuação
+#
+# Veja o código no editor. O que acontecerá quando o executar? Verifique.
+#
+# Verá a seguinte mensagem em resposta:
+# Traceback (most recent call last):
+# File "lst.py", line 2, in 
+# x = list[0]
+# IndexError: list index out of range
+#
+# output
+#
+# Este é o IndexError.
+
+my_list = []
+x = my_list[0]
