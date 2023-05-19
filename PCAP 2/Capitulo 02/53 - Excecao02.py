cf2f0f93bf51ae04986514d174819dcf7728c562
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-18 21:47:05 -0300

    Adicionado o arquivo 53 na pasta Capitulo 02

---
+++ PCAP 2/Capitulo 02/53 - Excecao02.py
@@ -0,0 +1,17 @@
+# Exceções: continuação
+#
+# Veja o código no editor. Execute o programa (obviamente incorreto).
+#
+# Verá a seguinte mensagem em resposta:
+# Traceback (most recent call last):
+# File "div.py", line 2, in 
+# value /= 0
+# ZeroDivisionError: division by zero
+#
+# output
+#
+# Este erro de exceção é chamado ZeroDivisionError.
+
+
+value = 1
+value /= 0
