a436ac0df63005381bfa075f62359d4278507ceb
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-30 14:38:39 -0300

    Adicionado o arquivo 49 na pasta Capitulo 04

---
+++ PCAP 2/Capitulo 04/49 - Datetame.py
@@ -0,0 +1,21 @@
+
+Introdução ao módulo datetime .
+
+Nesta secção, aprenderá sobre um módulo Python chamado datetime.
+
+Como pode adivinhar, proporciona classes para trabalhar com data e hora. Se pensa que não precisa de aprofundar este tópico, falemos de exemplos de utilização de data e hora na programação.
+
+Data e hora têm inúmeras utilizações e é provavelmente difícil encontrar uma aplicação de produção que não as utilize. Aqui estão alguns exemplos:
+
+    registo de eventos - graças ao conhecimento da data e hora, somos capazes de determinar quando ocorre exatamente um erro crítico na nossa aplicação. Ao criar registos, pode especificar o formato de data e hora;
+    reastreamento de alterações na base de dados - por vezes é necessário armazenar informação sobre quando um registo foi criado ou modificado. O módulo datetime será perfeito para este caso;
+    validação de dados - em breve aprenderá como ler a data e hora atuais em Python. Sabendo a data e hora atuais, podemos validar vários tipos de dados, por exemplo, se um cupão de desconto introduzido por um utilizador na nossa aplicação ainda é válido;
+    armazenar informação importante - consegue imaginar transferências bancárias sem o armazenamento da informação de quando foram feitas? A data e a hora de certas ações devem ser preservadas, e nós temos de lidar com isso.
+
+
+
+
+Uma cobra e uma ampulheta
+
+
+A data e hora são utilizadas em quase todas as áreas das nossas vidas, por isso é importante familiarizar-se com o módulo Python datetime. Está pronto para uma nova dose de conhecimento?
