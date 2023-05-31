c8abbafefd9f5ab71c5c9e453697dbc2ac0ad717
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2023-05-30 21:29:37 -0300

    Adicionado o arquivo 59 na pasta Capitulo 04

---
+++ PCAP 2/Capitulo 04/59 - Datetime11.py
@@ -0,0 +1,32 @@
+# Os loops asctime() e mktime() .
+#
+# O módulo time tem funções que esperam um objeto struct_time ou um tuple que armazena valores de acordo com os indexes apresentados quando se discute a classe struct_time. Execute o exemplo no editor.
+#
+# Resultado:
+# Mon Nov  4 14:53:00 2019
+# 1572879180.0
+#
+# output
+#
+# A primeira das funções, chamada asctime, converte um objeto struct_time ou um tuple numa string. Note-se que a função familiar gmtime é usada para obter o objeto struct_time. Se não fornecer um argumento à # função asctime , o tempo devolvido pela função localtime será usado.
+#
+# A segunda função chamada mktime converte um objeto struct_time ou um tuple que expressa a hora local para o número de segundos desde a época Unix. No nosso exemplo, passamos-lhe um tuple, que consiste nos 
+# seguintes valores:
+#
+# 2019 => tm_year
+# 11 => tm_mon
+# 4 => tm_mday
+# 14 => tm_hour
+# 53 => tm_min
+# 0 => tm_sec
+# 0 => tm_wday
+# 308 => tm_yday
+# 0 => tm_isdst
+
+import time
+
+timestamp = 1572879180
+st = time.gmtime(timestamp)
+
+print(time.asctime(st))
+print(time.mktime((2019, 11, 4, 14, 53, 0, 0, 308, 0)))
